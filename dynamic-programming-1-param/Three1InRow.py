e8c0c6865999560ffff4d413ecefed89718dbee4
vkoschman <207955@niuitmo.ru>
2023-03-08 17:10:50 +0300

    added Three1InRow problem

---
+++ dynamic-programming-1-param/Three1InRow.py
@@ -0,0 +1,48 @@
+def compute_sequences():
+    sequence_len = read_input()
+    sequence_number = get_sequence_number(sequence_len)
+    write_output(sequence_number)
+
+
+def read_input():
+    with open("input.txt") as input:
+        lines = input.readlines()
+    return int(lines[0])
+
+
+# My initial solution
+# O(n) time | O(1) space
+def get_sequence_number_1(n):
+    if n <= 2:
+        return n * 2
+    if n == 3:
+        return 2 ** 3 - 1
+    counts = [2, 4, 7, 0]
+    for i in range(3, n):
+        invalid_num = 1 if i < 4 else counts[i % 4]
+        counts[i % 4] = counts[(i - 1) % 4] * 2 - invalid_num
+    return counts[n % 4 - 1]
+
+
+# Solution suggested by lecturer
+# O(n) time | O(n) space
+def get_sequence_number(n):
+    if n <= 2:
+        return n * 2
+    if n == 3:
+        return 2 ** 3 - 1
+    dp = [0] * n
+    dp[0] = 2
+    dp[1] = 4
+    dp[2] = 7
+    for i in range(3, n):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[-1]
+
+
+def write_output(result):
+    with open('output.txt', 'w') as output:
+        output.write(str(result))
+
+
+compute_sequences()
